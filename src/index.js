7f24eac89a7112151b3ba792042be756d1e1fe3f
Samir Said <97880758+sairsaid@users.noreply.github.com>
2023-02-03 17:56:30 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 const container = document.getElementById('root');
 const root = createRoot(container);
 
+
 root.render(
   <React.StrictMode>
     <Provider store={store}>
